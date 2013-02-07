c625d142c0667153b6f7bde43c47d1620132b9ca
kitsully <kris@kristophersullivan.com>
2013-02-07 15:02:26 -0500

    Outlined the Block Class Data Structure
    Signed-off-by: kitsully <kris@kristophersullivan.com>

---
+++ blockLayer.py
@@ -0,0 +1,18 @@
+# Kristopher Sullivan 
+# Operating Systems
+# February 7, 2013 
+
+# Block Layer
+
+class Block:
+	block_size = 512
+	def write(block_start, buffer, buffer_start, num_bytes):
+
+	def read(block_start, buffer, buffer_start, num_bytes):
+
+	def block_size():
+		return self.block_size
+	
+
+
+
