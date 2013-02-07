653e42a4df6f56197773456f349fb48ff7af9f8e
kitsully <kris@kristophersullivan.com>
2013-02-07 16:07:21 -0500

    Got Write Working
    We think we got the write to block method working
    
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- blockLayer.py
+++ blockLayer.py
@@ -4,15 +4,37 @@
 
 # Block Layer
 
+from array import array
+
+data = []
+
 class Block:
-	block_size = 512
-	def write(block_start, buffer, buffer_start, num_bytes):
+	size = 512
+	buff = buffer(" ", 0, 0)
+	def write(self, block_start, buff1, buffer_start, num_bytes):
+		if (num_bytes < 512):
+			buff = buffer(buff1, buffer_start, num_bytes)
+			print buff
+		else:
+			print "error"
+		return 0
+
 
 	def read(block_start, buffer, buffer_start, num_bytes):
+		print "Test"
+		return 0
 
-	def block_size():
+
+	def block_size(self):
+		print "hello"
 		return self.block_size
-	
+
+
+b = Block()
 
 
 
+
+
+# Example of how to write to a block
+b.write(0, "Hello World", 7, 511)
