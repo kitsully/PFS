e8e527e69261c568d85af28fcd25ed1f2203d747
kitsully <kris@kristophersullivan.com>
2013-02-08 11:37:20 -0500

    Finished Write method for Block Layer
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- blockLayer.py
+++ blockLayer.py
@@ -8,13 +8,20 @@ class Block:
 	size = 512
 	blk = ["0"] * 512  # This creates a list with 512 spaces 
 	def write(self, block_start, buff1, buffer_start, num_bytes):
-		buf_i = buffer_start # I am not really sure why this is needed >< 
+		buf_i = buffer_start 
 		for i in range(block_start, block_start + num_bytes): 
-			n = i
-			if (n < len(buff1)): # makes sure that we do not get an array out of bounds error
-				self.blk[i] = buff1[n] # sets the positon in the block equal to the posiiton in the string
-			i += 1 # increments i
-		return 0
+			# checks if block can be written too
+			if(i < self.size):
+				self.blk[i] = buff1[buf_i]
+			else:
+				return 1 # failure if full text was not written
+			# Checks for index out of bound problem
+			if(buf_i < num_bytes): 
+				buf_i += 1
+			else:
+				return 0 # success
+			i += 1
+		return 1 # return failure by default
 
 
 	def read(block_start, buffer, buffer_start, num_bytes):
@@ -34,5 +41,11 @@ b = Block()
 
 
 # Example of how to write to a block
-b.write(0, "Hello World", 0, 511) 
-b.block_size() # this prints out the whole list the first few spaces should be equal to hello world
+b.write(0, "Hello World", 0, 11) 
+# b.block_size() # this prints out the whole list the first few spaces should be equal to hello world
+
+
+s = "I think I did it!"
+#b.write(35, s, 0, 17)
+
+b.block_size()
