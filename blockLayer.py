6d563f32e70210377c69b0482faaa2d14ad46a36
kitsully <kris@kristophersullivan.com>
2013-02-08 08:37:11 -0500

    Added some comments for Anita
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- blockLayer.py
+++ blockLayer.py
@@ -6,14 +6,14 @@
 
 class Block:
 	size = 512
-	blk = ["0"] * 512
+	blk = ["0"] * 512  # This creates a list with 512 spaces 
 	def write(self, block_start, buff1, buffer_start, num_bytes):
-		buf_i = buffer_start
-		for i in range(block_start, block_start + num_bytes):
+		buf_i = buffer_start # I am not really sure why this is needed >< 
+		for i in range(block_start, block_start + num_bytes): 
 			n = i
-			if (n < len(buff1)):
-				self.blk[i] = buff1[n]
-			i += 1
+			if (n < len(buff1)): # makes sure that we do not get an array out of bounds error
+				self.blk[i] = buff1[n] # sets the positon in the block equal to the posiiton in the string
+			i += 1 # increments i
 		return 0
 
 
@@ -34,5 +34,5 @@ b = Block()
 
 
 # Example of how to write to a block
-b.write(0, "Hello World", 0, 511)
-b.block_size()
+b.write(0, "Hello World", 0, 511) 
+b.block_size() # this prints out the whole list the first few spaces should be equal to hello world
