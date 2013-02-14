70dccfcb5358d9887a4a65a20a73b9884a85b904
kitsully <kris@kristophersullivan.com>
2013-02-14 14:20:37 -0500

    added inode_number_to_inode procedure
    not tested not syntax error
    
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- inode_number.py
+++ inode_number.py
@@ -15,5 +15,15 @@ for i in range(0, _num_inodes):
 	_inode_table.append(INode(inode_type = FileType.regular_file))
 
 
+def valid_inode(inode_num):
+	return (inode_num >= 0) and (inode_num < _num_inodes)
+
+def inode_number_to_inode(inode_num):
+	if valid_inode(inode_num):
+		return _inode_table[inode_num]
+	raise Exception("Inode number %s out of range." % inode_num)
+
+
+
 if __name__ == '__main__':
 	print _inode_table
