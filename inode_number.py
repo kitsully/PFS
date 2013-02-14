8e565b516ba849b7ef6d89a0c0530c2efded8d85
kitsully <kris@kristophersullivan.com>
2013-02-14 14:16:39 -0500

    created a table of inode objects
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- inode_number.py
+++ inode_number.py
@@ -5,7 +5,15 @@
 # inode Number
 
 
-import blockLayer
 import fileLayer
+from fileLayer import INode, FileType
 
-_inode_table
+_num_inodes = 100
+_inode_table = []
+
+for i in range(0, _num_inodes):
+	_inode_table.append(INode(inode_type = FileType.regular_file))
+
+
+if __name__ == '__main__':
+	print _inode_table
