adf751be5486ad6650d1e8f509060f33eed621ce
kitsully <kris@kristophersullivan.com>
2013-02-14 15:33:26 -0500

    Changed My Block Layer to match Bradshaws
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- fileLayer.py
+++ fileLayer.py
@@ -4,64 +4,37 @@
 
 # File Layer
 
-import blockLayer
-
+import block
 
 _num_blocks_in_file = 100
 
-
-# c = blockLayer.get_free_block()
-
-# c.set_size(400)
-# print c.block_size()
-
-
-
 class FileType(object):
-	regular_file = 1
-	directory = 2
-		
-
-
+    regular_file = 1
+    directory = 2
 
 class INode(object):
-	def __init__(self, inode_type = FileType.regular_file):
-		self.blocks = _num_blocks_in_file * [-1] # the max number of blocks per file
-		self.size = 0
-		self.inode_type = inode_type
-	
+    def __init__(self, inode_type=FileType.regular_file):
+        self.blocks = _num_blocks_in_file * [-1]
+        self.size = 0
+        self.inode_type = inode_type
+        
+    def valid_index(self, index):
+        return True
 
-		def valid_index(self, index):
-			return True
-
-
-	# iNode = ['_'] * max_number_blocks_per_file  # the list holding the files data in sequence
-	# size = 0  # the maximum size of the iNode
-
-	# Procedures ------
-
-	def index_to_block_number(self, index):
-		if self.valid_index(index):
-			return self.blocks[index]
-		raise Exception("Index number %s out of range." % index)
-
-	# def inode_to_block(self, byte_offset):
-	# 	o = byte_offset / c.block_size()
-	# 	b = self.index_to_block_number(o)
-	# 	return c.block_number_to_block(b)
-
-
+    def index_to_block_number(self, index):
+        if self.valid_index(index):
+            return self.blocks[index]
+        raise Exception("Index number %s out of range." % index)
 
 if __name__ == '__main__':
-	inode = INode()
-	print inode.blocks
-	print inode.size
-	print inode.inode_type
+    print "File inode:"
+    inode = INode()
+    print inode.blocks
+    print inode.size
+    print inode.inode_type
 
-
-
-
-
-# node = INode()
-
-# print node.inode_to_block(30)
+    print "Directory inode:"
+    inode = INode(inode_type=FileType.directory)
+    print inode.blocks
+    print inode.size
+    print inode.inode_type
