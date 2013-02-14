9c773fd5e5c0a925e5fc956096dfaf8b25822c89
kitsully <kris@kristophersullivan.com>
2013-02-14 14:02:48 -0500

    In Class changes1
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- fileLayer.py
+++ fileLayer.py
@@ -7,8 +7,31 @@
 import blockLayer
 
 
-class iNode:
-	max_number_blocks_per_file = 100  # the max number of blocks per file
+_num_blocks_in_file = 100
+
+
+c = blockLayer.get_free_block()
+
+c.set_size(400)
+print c.block_size()
+
+
+
+class FileType(object):
+	regular_file = 1
+	directory = 2
+		
+
+
+
+class INode(object):
+	def __init__(self, input_type = FileType.regular_file):
+		self.blocks = _num_blocks_in_file * [-1] # the max number of blocks per file
+		self.size = 0
+		self.inode_type
+	
+
+
 	iNode = ['_'] * max_number_blocks_per_file  # the list holding the files data in sequence
 	size = 0  # the maximum size of the iNode
 
@@ -18,7 +41,22 @@ class iNode:
 		return self.iNode[index]
 
 	def inode_to_block(self, byte_offset):
-		o = byte_offset / Block.block_size()
+		o = byte_offset / c.block_size()
 		b = self.index_to_block_number(o)
-		return Block.block_number_to_block(b)
+		return c.block_number_to_block(b)
 
+
+
+if __name__ == '__main__':
+	inode = INODE()
+	print inode.blocks
+	print inode.size
+	print inode.inode_type
+
+	
+
+
+
+node = INode()
+
+print node.inode_to_block(30)
