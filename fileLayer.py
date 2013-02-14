e1125f55caca0da5f7f0d4f20862de3e94b45010
kitsully <kris@kristophersullivan.com>
2013-02-14 16:52:34 -0500

    added two methods
    - addBlock
    	-tests the adding of a block to the inode table
    
    -inode_number_to_block
    
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- fileLayer.py
+++ fileLayer.py
@@ -21,14 +21,41 @@ class INode(object):
     def valid_index(self, index):
         return True
 
+    def add_block(self, index, block):
+    	self.blocks[index] = block
+
     def index_to_block_number(self, index):
         if self.valid_index(index):
             return self.blocks[index]
         raise Exception("Index number %s out of range." % index)
 
+
+    def inode_number_to_block(offset, inode_number):
+    	inode = index_to_block_number(inode_number)
+    	o = offset / block.block_size()
+    	b = inode.index_to_block_number(o)
+    	return block.index_to_block_number(b)
+
+
+
+
+
+# inode = INode()
+
+# b = blockLayer.Block()
+# #b.block_size()
+
+# inode.add_block(2, b)
+# print inode.valid_index(1)
+
+
+
 if __name__ == '__main__':
     print "File inode:"
     inode = INode()
+    b = blockLayer.Block()
+    inode.add_block(2, b)
+    inode.inode_number_to_block()
     print inode.blocks
     print inode.size
     print inode.inode_type
