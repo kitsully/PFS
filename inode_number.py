6f0423cd608927f42c8f9a8883161d7f0be807c1
kitsully <kris@kristophersullivan.com>
2013-02-15 10:30:44 -0500

    Added inode_number_to_block method
    I think it is working not sure how to test it
    
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- inode_number.py
+++ inode_number.py
@@ -6,6 +6,7 @@
 
 
 import fileLayer
+import blockLayer
 from fileLayer import INode, FileType
 
 _num_inodes = 100
@@ -21,9 +22,20 @@ def valid_inode(inode_num):
 def inode_number_to_inode(inode_num):
 	if valid_inode(inode_num):
 		return _inode_table[inode_num]
-	raise Exception("Inode number %s out of range." % inode_num)
+	else:
+		raise Exception("Inode number %s out of range." % inode_num)
+
+def inode_number_to_block(offset, inode_num):
+	inode = inode_number_to_inode(inode_num)
+	o = offset / blockLayer.get_block_size()
+	b = inode.index_to_block_number(o)
+	return blockLayer.block_number_to_block(b)
 
 
 
 if __name__ == '__main__':
-	print _inode_table
+	# print _inode_table
+
+	print "=====", _inode_table[1]
+	print "=====", inode_number_to_block(512, 1)
+
