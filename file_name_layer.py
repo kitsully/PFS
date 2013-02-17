fb36b06bb44975ff271e2bff71cb5d755ccdd0a5
kitsully <kris@kristophersullivan.com>
2013-02-17 13:32:36 -0500

    added inode_num function and tested it
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- file_name_layer.py
+++ file_name_layer.py
@@ -9,7 +9,7 @@ import blockLayer
 from fileLayer import INode, FileType
 import inode_number
 
-# _wd = ""  # the working directory
+
 
 
 def lookup(filename, directory):
@@ -18,19 +18,47 @@ def lookup(filename, directory):
 	if (i.inode_type != FileType.directory):
 		raise Exception ("Not a directory.")
 	offset = 0
-	while offset < i.size:
+	while (offset < i.size):
 		b = blockLayer.inode_number_to_block(offset,  directory)
 		if (string_match(filename, b):
 			return inode_number(filename, b)
 		offset = offset + blockLayer.get_block_size()
 	raise Exception("Error")
 
-
 def createDict(data):
-	data = test.split(",")
+	b = data.split(",")
 	dic = {}
 	l = []
 	for item in b:
 		part = item.split("|")
 		dic.update({part[0]:part[1]})
 	return dic
+
+def string_match(filename, b):
+	buf = [''] * blockLayer._block_size
+	buf = b.read(0, buf, 0, b.max_capacity)
+	d = "".join(buf)
+	dic = createDict(d)
+	for item in dic:
+		if (filename == item):
+			return True
+		else:
+			return False
+
+def inode_number(filename, b):
+	buf = [''] * blockLayer._block_size
+	buf = b.read(0, buf, 0, b.max_capacity)
+	d = "".join(buf)
+	dic = createDict(d)
+	for item in dic:
+		if (filename == item):
+			print dic.get(filename)
+		else:
+			raise Exception("%s file is not in directory." % filename)
+
+
+# c = blockLayer.Block()
+# s = "Test|12,red|10"
+# c.write(0, s, 0, len(s))
+# inode_number("Test", c)
+
