5214dfeccd4335e947c32a2213c42f6a83dccd9c
kitsully <kris@kristophersullivan.com>
2013-02-18 13:12:17 -0500

    Added some comments
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- path_name_layer.py
+++ path_name_layer.py
@@ -10,6 +10,8 @@ from fileLayer import INode, FileType
 import inode_number
 import file_name_layer
 
+_wd = 0
+
 def plain_name(path): # scans its argument for the UNIX path name separator (forward slash).
 	if (path.find("/") == -1):
 		return True
@@ -25,10 +27,22 @@ def rest(path): # returns the remainder of the path name
 	loc = path.find("/")
 	return path[loc:]
 
-def path_to_inode_number(path, directory):
+def path_to_inode_number(path, directory): # takes a path its inode number
 	if (plain_name(path)):
 		return file_name_layer.name_to_inode_number(path, directory)
 	else:
 		directory = file_name_layer.lookup(first(path), directory)
 		path = rest(path)
 		return path_to_inode_number(path, directory)
+
+def chdir(path):
+	global _wd
+	_wd = path_to_inode_number(path, wd)
+	return 0
+
+if __name__ == '__main__':
+	i = inode_number.inode_number_to_inode(2)	
+	i.inode_type = FileType.directory
+	p = "right/wrong/left"
+	test 
+
