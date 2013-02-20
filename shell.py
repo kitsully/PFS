0aa264cb63532bd8b55465faa4a084a34254766a
kitsully <kris@kristophersullivan.com>
2013-02-20 09:01:51 -0500

    Fixed ls to return a message if a directory is empty updated code to match new file structure
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- shell.py
+++ shell.py
@@ -12,16 +12,18 @@ import file_name_layer
 import path_name_layer
 import absolute_path_name_layer
 
-path_name_layer.chdir("/dir")
 
 def ls(): # lists the contents of the working directory
 	b = inode_number.inode_number_to_block((inode_number.inode_number_to_inode(path_name_layer._wd)).size, path_name_layer._wd)
 	buf = [''] * b.size
 	buf = b.read(0, buf, 0, b.size - 1)
 	d = "".join(buf)
-	dic = file_name_layer.createDict(d)
-	for key in dic.keys():
-		print "- ", key
+	if(0 != b.size):
+		dic = file_name_layer.createDict(d)
+		for key in dic.keys():
+			print "- ", key
+	else:
+		print "Empty Directory"
 
 def mkdir(directory): # creates a directory in the working directory
 	file_name_layer.mkdir(directory, path_name_layer._wd)
@@ -31,14 +33,33 @@ def cd(path): # change directory
 	path_name_layer.chdir(path)
 
 
+def cat(filename): # Prints out the contents of a file
+	num_blocks = 0
+	pointer = 0
+	count = 0
+	inum = file_name_layer.lookup(filename, path_name_layer._wd)
+	i = inode_number.inode_number_to_inode(inum)
+	for b in i.blocks:
+		if (b != -1):
+			num_blocks += 1
+	# print "-----", num_blocks		
+	buf = [""] * (blockLayer._block_size * num_blocks)
+	while(pointer < num_blocks):
+		block = blockLayer.block_number_to_block(i.blocks[pointer])
+		s = "dfdfdfdfd"
+		block.write(0, s, 0, len(s) )
+		buf = block.read(0, buf, count, block.size)
+		pointer += 1
+		count += blockLayer._block_size 
+	buf = "".join(buf)
+	print buf
+ 
 
 
 
-
-# mkdir("hello")
-cd("/")
-ls()
-print "----"
-cd("/dir")
+# cd("/")
+# ls()
+# print "----"
+cd("/Home/Kris/Music")
 ls()
  
