5add27e08991ddb7868eaec376925509de270bca
kitsully <kris@kristophersullivan.com>
2013-02-21 11:50:30 -0500

    Linted the path name layer
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- path_name_layer.py
+++ path_name_layer.py
@@ -4,65 +4,65 @@
 
 # path_name
 
-import fileLayer
-import blockLayer
-from fileLayer import INode, FileType
-import inode_number
 import file_name_layer
 
 global _wd # global variable to hold the working directory 
 _wd = 0
 
-def plain_name(path): # scans its argument for the UNIX path name separator (forward slash).
-	if (path.find("/") == -1):
-		return True
-	else:
-		return False
 
-def first(path): # returns the first component name
-	loc = path.split("/")
-	return loc[0]
+# scans its argument for the UNIX path name separator (forward slash).
+def plain_name(path): 
+    if (path.find("/") == -1):
+        return True
+    else:
+        return False
 
-def rest(path): # returns the remainder of the path name
-	loc = path.split("/", 1)
-	return loc[1]
 
-def path_to_inode_number(path, directory): # takes a path its inode number
-	# print "llllllllllll", path
-	if (plain_name(path)):
-		# print "***********************()"
-		return file_name_layer.name_to_inode_number(path, directory)
-	else:
-		# print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-		directory = file_name_layer.lookup(first(path), directory)
-		path = rest(path)
-		return path_to_inode_number(path, directory)
+# returns the first component name
+def first(path): 
+    loc = path.split("/")
+    return loc[0]
 
-def chdir(path): # changes the working directory 
-	global _wd
-	# print "path :", path
-	if (path == "/"):
-		global _wd
-		_wd = 0
-		return 1 #success
-	elif(path[0] == "/"):
-		# print "&&&&&&&&"
-		global _wd
-		_wd = path_to_inode_number(path[1:], 0)
-		# print "::::::::::::::::::", path[1:]
-		return 1
-	global _wd 
-	_wd = path_to_inode_number(path, _wd)
-	return 1
+
+# returns the remainder of the path name
+def rest(path): 
+    loc = path.split("/", 1)
+    return loc[1]
+
+
+# takes a path its inode number
+def path_to_inode_number(path, directory): 
+    if (plain_name(path)):
+        return file_name_layer.name_to_inode_number(path, directory)
+    else:
+        directory = file_name_layer.lookup(first(path), directory)
+        path = rest(path)
+        return path_to_inode_number(path, directory)
+
+
+# changes the working directory 
+def chdir(path): 
+    global _wd
+    if (path == "/"):
+        global _wd
+        _wd = 0
+        return 1 #success
+    elif(path[0] == "/"):
+        global _wd
+        _wd = path_to_inode_number(path[1:], 0)
+        return 1
+    global _wd 
+    _wd = path_to_inode_number(path, _wd)
+    return 1
 
 
 
 #if __name__ == '__main__':
-	# global _wd
-	# _wd = 12
-	# print "----", _wd	
-	# i = inode_number.inode_number_to_inode(2)	
-	# i.inode_type = FileType.directory
-	# p = "right/wrong/left"
-	# test 
+    # global _wd
+    # _wd = 12
+    # print "----", _wd 
+    # i = inode_number.inode_number_to_inode(2) 
+    # i.inode_type = FileType.directory
+    # p = "right/wrong/left"
+    # test 
 
