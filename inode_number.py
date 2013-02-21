d095903ccd89f34cbfe38eaee1e12a28c77b9ccf
kitsully <kris@kristophersullivan.com>
2013-02-21 11:27:04 -0500

    Linted inode number file
    Signed-off-by: kitsully <kris@kristophersullivan.com>

--- inode_number.py
+++ inode_number.py
@@ -4,8 +4,6 @@
 
 # inode Number
 
-
-import fileLayer
 import blockLayer
 from fileLayer import INode, FileType
 
@@ -15,44 +13,53 @@ _inode_table = []
 _free_inodes = [0] * _num_inodes
 
 
-for i in range(0, _num_inodes): # creates a table of free inodes
-	_inode_table.append(INode(inode_type = FileType.regular_file))
+for i in range(0, _num_inodes):  # creates a table of free inodes
+    _inode_table.append(INode(inode_type = FileType.regular_file))
 
-_free_inodes[0] = 1 # marks inode as used
+_free_inodes[0] = 1  # marks inode as used
 
-def change_type(num): # changes inode type to directory
-	_inode_table[num].inode_type = FileType.directory
+# changes the inode type to directory
+def change_type(num): 
+    _inode_table[num].inode_type = FileType.directory
 
-change_type(0) # reserves the root directory
+change_type(0)  # reserves the root directory
 
-def release_inode(num): # frees an inode
-	if valid_inode(num):
-	    if(_free_inodes[num] == 0):  # checks if free
-	        print "Already Free"
-	    else: 
-	        _free_inodes[num] = 0  # Changes inode status to free 
 
-def get_free_inode(): # returns the index of a free inode
+# frees an inode
+def release_inode(num):
+    if valid_inode(num):
+        if(_free_inodes[num] == 0):  # checks if free
+            print "Already Free"
+        else: 
+            _free_inodes[num] = 0  # Changes inode status to free 
+
+
+# returns the index of a free inode
+def get_free_inode():
     for i in range(1, _num_inodes - 1):
         if(_free_inodes[i] == 0): 
             _free_inodes[i] = 1  
             return i
     raise Exception("No more free inodes.")
 
-def valid_inode(inode_num): # checks to see if the inode number is valid
-	return (inode_num >= 0) and (inode_num < _num_inodes)
+
+# checks to see if the inode number is valid
+def valid_inode(inode_num): 
+    return (inode_num >= 0) and (inode_num < _num_inodes)
+
 
 def inode_number_to_inode(inode_num): 
-	if valid_inode(inode_num):
-		return _inode_table[inode_num]
-	else:
-		raise Exception("Inode number %s out of range." % inode_num)
+    if valid_inode(inode_num):
+        return _inode_table[inode_num]
+    else:
+        raise Exception("Inode number %s out of range." % inode_num)
+
 
 def inode_number_to_block(offset, inode_num):
-	inode = inode_number_to_inode(inode_num)
-	o = offset / blockLayer.get_block_size()
-	b = inode.index_to_block_number(o)
-	return blockLayer.block_number_to_block(b)
+    inode = inode_number_to_inode(inode_num)
+    o = offset / blockLayer.get_block_size()
+    b = inode.index_to_block_number(o)
+    return blockLayer.block_number_to_block(b)
 
 
 
@@ -64,5 +71,3 @@ def inode_number_to_block(offset, inode_num):
 #     print _inode_table[0]    
 #     test = inode_number_to_block(0, 0)
 #     print test
-
-
